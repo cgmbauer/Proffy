11aadc3c179bf051d714ec01d29be2b6ce9464ad
cgmbauer <cgmbauer@gmail.com>
2020-08-04 01:54:45 -0300

    palette of colors

---
+++ frontend-web/src/styles/colors.ts
@@ -0,0 +1,22 @@
+const colors = {
+  colorBackground: '#F0F0F7',
+  colorPrimaryLighter: '#9871F5',
+  colorPrimaryLight: '#916BEA',
+  colorPrimary: '#8257E5',
+  colorPrimaryDark: '#774DD6',
+  colorPrimaryDarker: '#6842C2',
+  colorSecundary: '#04D361',
+  colorSecundaryDark: '#04BF58',
+  colorTitleInPrimary: '#FFFFFF',
+  colorTextInPrimary: '#D4C2FF',
+  colorTextTitle: '#32264D',
+  colorTextComplement: '#9C98A6',
+  colorTextBase: '#6A6180',
+  colorLineInWhite: '#E6E6F0',
+  colorInputBackground: '#F8F8FC',
+  colorButtonText: '#FFFFFF',
+  colorBoxBase: '#FFFFFF',
+  colorBoxFooter: '#FAFAFC',
+};
+
+export default colors;
