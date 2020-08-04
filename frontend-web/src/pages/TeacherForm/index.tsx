36007d7e661a102e1db71f193d66f66261ae8be8
cgmbauer <cgmbauer@gmail.com>
2020-08-04 01:53:22 -0300

    basic structure done

---
+++ frontend-web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+const TeacherForm: React.FC = () => {
+  return <PageHeader title="Que incrível que você quer dar aulas." />;
+};
+
+export default TeacherForm;
