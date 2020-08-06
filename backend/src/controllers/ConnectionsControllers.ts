fc4d906b26ffd0d2bad163d5a853a78389f3a1a2
cgmbauer <cgmbauer@gmail.com>
2020-08-06 18:04:28 -0300

    changed return type

--- backend/src/controllers/ConnectionsControllers.ts
+++ backend/src/controllers/ConnectionsControllers.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import db from '../database/connection';
 
 export default class ConnectionsController {
-  async index(request: Request, response: Response): Promise<void> {
+  async index(request: Request, response: Response): Promise<Response> {
     const totalConnections = await db('connections').count('* as total');
 
     const { total } = totalConnections[0];
